be235f5a9b027860664d339cd317de1de1933c1c
tien.xuan.vo <tien.xuan.vo@gmail.com>
2019-09-09 09:39:25 +0700

    Add register

--- src/register.php
+++ src/register.php
@@ -6,10 +6,16 @@ use Facebook\WebDriver\Remote\DesiredCapabilities;
 use Facebook\WebDriver\Remote\WebDriverCapabilityType;
 use Facebook\WebDriver\Remote\WebDriverBrowserType;
 use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\WebDriverBy;
+use Facebook\WebDriver\WebDriverExpectedCondition;
 
 $caps = new DesiredCapabilities();
 $caps->setCapability(WebDriverCapabilityType::BROWSER_NAME, WebDriverBrowserType::CHROME);
-$caps->setCapability(WebDriverCapabilityType::VERSION, '76.0');
+$caps->setCapability(WebDriverCapabilityType::VERSION, '75.0');
+$caps->setCapability('selenoid:options', [
+    WebDriverCapabilityType::BROWSER_NAME => WebDriverBrowserType::ANDROID,
+    'skin' => 'WXGA720'
+]);
 // These capabilities are for Selenoid only
 $caps->setCapability('enableVNC', true);
 $caps->setCapability('enableLog', false);
@@ -19,9 +25,9 @@ $driver = RemoteWebDriver::create('http://localhost:4444/wd/hub', $caps);
 
 $url = 'https://www.phptravels.net/';
 
-$driver->get($url.'login');
+$driver->get($url.'register');
 $driver->wait(10, 1000)->until(
-    WebDriverExpectedCondition::visibilityOfElementLocated(WebDriverBy::id('loginfrm'))
+    WebDriverExpectedCondition::visibilityOfElementLocated(WebDriverBy::id('headersignupform'))
 );
 
 $driver->quit();
