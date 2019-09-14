410dc0c5c1b16e959797524b0e53c31dbac936c4
Tham Vu <thamvu107@gmail.com>
2019-09-14 20:47:02 +0700

    Click action

--- src/login.php
+++ src/login.php
@@ -9,6 +9,7 @@ use Facebook\WebDriver\Remote\RemoteWebDriver;
 use Facebook\WebDriver\WebDriverExpectedCondition;
 use Facebook\WebDriver\WebDriverBy;
 use Facebook\WebDriver;
+use Facebook\WebDriver\WebDriverDimension;
 
 $caps = new DesiredCapabilities();
 $caps->setCapability(WebDriverCapabilityType::BROWSER_NAME, WebDriverBrowserType::CHROME);
@@ -18,15 +19,20 @@ $caps->setCapability('enableVNC', true);
 $caps->setCapability('enableLog', false);
 $caps->setCapability('enableVideo', true);
 
-$webDriver = RemoteWebDriver::create('http://localhost:4444/wd/hub', $caps);
+$driver = RemoteWebDriver::create('http://localhost:4444/wd/hub', $caps);
+// $driver.manage().window().setSize(new Dimension(1024,768));
+// $driver->manage()->window()->setSize(new WebDriverDimension(1024, 768));
+
 
 $baseUrl = 'https://www.phptravels.net/';
-$invalidEmail = 'xinh.xing@gmail.com';
+$invalidEmail = 'invalid.g@gmail.com';
 $password = 'zaq12wsx';
 
-$webDriver->get($baseUrl);
-$webDriver->wait(10, 1000)->until(
-    WebDriverExpectedCondition::visibilityOfElementLocated(WebDriverBy::id('li_myaccount'))
- );
+$driver->get($baseUrl);
+$driver->manage()->window()->setSize(new WebDriverDimension(1024, 768));
+$driver->wait(10, 1000)->until(
+    $$driver->findElement(WebDriverBy::xpath("(.//*[normalize-space(text()) and normalize-space(.)='My Account'])[2]/following::a[1]"))->click()
+);
+
 
 $driver->quit();
