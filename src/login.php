f3018068a5500dee870b5f905667a1c0725b9e22
tien.xuan.vo <tien.xuan.vo@gmail.com>
2019-09-14 22:17:33 +0700

    Change website

--- src/login.php
+++ src/login.php
@@ -8,8 +8,6 @@ use Facebook\WebDriver\Remote\WebDriverBrowserType;
 use Facebook\WebDriver\Remote\RemoteWebDriver;
 use Facebook\WebDriver\WebDriverExpectedCondition;
 use Facebook\WebDriver\WebDriverBy;
-use Facebook\WebDriver;
-use Facebook\WebDriver\WebDriverDimension;
 
 $caps = new DesiredCapabilities();
 $caps->setCapability(WebDriverCapabilityType::BROWSER_NAME, WebDriverBrowserType::CHROME);
@@ -20,17 +18,21 @@ $caps->setCapability('enableLog', false);
 $caps->setCapability('enableVideo', true);
 
 $driver = RemoteWebDriver::create('http://localhost:4444/wd/hub', $caps);
-// $driver.manage().window().setSize(new Dimension(1024,768));
-// $driver->manage()->window()->setSize(new WebDriverDimension(1024, 768));
 
+$baseUrl = 'http://automationpractice.com';
+$email = 'abc@xyz.com';
+$password = 'Test@123';
 
-$baseUrl = 'https://www.phptravels.net/';
-$invalidEmail = 'invalid.g@gmail.com';
-$password = 'zaq12wsx';
-
+$driver->manage()->window()->maximize();
 $driver->get($baseUrl);
-$driver->manage()->window()->setSize(new WebDriverDimension(1024, 768));
 $driver->wait(10, 1000)->until(
-    $driver->findElement(WebDriverBy::xpath("(.//*[normalize-space(text()) and normalize-space(.)='My Account'])[2]/following::a[1]"))->click()
+    WebDriverExpectedCondition::visibilityOfAnyElementLocated(WebDriverBy::cssSelector('.login'))
 );
+
+$driver->findElement(WebDriverBy::cssSelector('.login'))->click();
+
+$driver->findElement(WebDriverBy::id('email'))->sendKeys($email);
+$driver->findElement(WebDriverBy::id('passwd'))->sendKeys($password);
+$driver->findElement(WebDriverBy::id('SubmitLogin'))->click();
+
 $driver->quit();
