5ffbb0d0808ec209e2bab1391e5f69dea4a60ee4
Tham Vu <thamvu107@gmail.com>
2019-10-05 13:03:43 +0700

    Login

--- src/login.php
+++ src/login.php
@@ -2,6 +2,7 @@
 
 require __DIR__.'/../vendor/autoload.php';
 
+use Webmozart\Assert\Assert;
 use Facebook\WebDriver\Remote\DesiredCapabilities;
 use Facebook\WebDriver\Remote\WebDriverCapabilityType;
 use Facebook\WebDriver\Remote\WebDriverBrowserType;
@@ -19,20 +20,38 @@ $caps->setCapability('enableVideo', true);
 
 $driver = RemoteWebDriver::create('http://localhost:4444/wd/hub', $caps);
 
-$baseUrl = 'http://automationpractice.com';
-$email = 'abc@xyz.com';
-$password = 'Test@123';
+$baseUrl = 'https://www.phptravels.net';
+$email = 'tham.vu@go1.com';
+$password = '1234567890';
 
 $driver->manage()->window()->maximize();
 $driver->get($baseUrl);
 $driver->wait(10, 1000)->until(
-    WebDriverExpectedCondition::visibilityOfAnyElementLocated(WebDriverBy::cssSelector('.login'))
+    WebDriverExpectedCondition::elementToBeClickable(WebDriverBy::cssSelector('.container .user_menu li a'))
 );
 
-$driver->findElement(WebDriverBy::cssSelector('.login'))->click();
+$driver->findElement(WebDriverBy::cssSelector('.container .user_menu li a'))->click();
 
-$driver->findElement(WebDriverBy::id('email'))->sendKeys($email);
-$driver->findElement(WebDriverBy::id('passwd'))->sendKeys($password);
-$driver->findElement(WebDriverBy::id('SubmitLogin'))->click();
+$driver->wait(10, 1000)->until(
+    WebDriverExpectedCondition::visibilityOfElementLocated(WebDriverBy::cssSelector('.container .user_menu li .dropdown-menu'))
+);
+
+$driver->findElement(WebDriverBy::cssSelector('.container .user_menu li .dropdown-menu a[href*="www.phptravels.net/login"]'))->click();
+
+$driver->wait(10, 1000)->until(
+    WebDriverExpectedCondition::visibilityOfElementLocated(WebDriverBy::cssSelector('input[name = username]'))
+);
+
+$driver->findElement(WebDriverBy::cssSelector('input[name = username]'))->sendKeys($email);
+$driver->findElement(WebDriverBy::cssSelector('input[name = password]'))->sendKeys($password);
+$driver->findElement(WebDriverBy::cssSelector('.loginbtn'))->click();
+$driver->wait(10, 1000)->until(
+    WebDriverExpectedCondition::urlIs('https://www.phptravels.net/account/')
+);
+
+Assert::eq(
+    'My Account',
+    $driver->getTitle()
+);
 
 $driver->quit();
